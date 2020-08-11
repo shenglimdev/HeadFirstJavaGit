5ef42a137cdb49fdd2d100c2428be1496ffef0c4
shenglimdev <69244497+shenglimdev@users.noreply.github.com>
2020-08-11 17:21:55 +1000

    Making first object

---
+++ Chapter2 - Trip to objectville/Dog/Dog.java
@@ -0,0 +1,12 @@
+package Dog;
+
+public class Dog {
+
+    int size;
+    String breed;
+    String name;
+
+    void bark() {
+        System.out.println("Ruff! Ruff!");
+    }
+}
---
+++ Chapter2 - Trip to objectville/Dog/DogTestDrive.java
@@ -0,0 +1,9 @@
+package Dog;
+
+public class DogTestDrive {
+    public static void main (String[] args){
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
