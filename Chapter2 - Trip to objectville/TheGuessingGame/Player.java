b0e9a51f5bc21e70704651316de9d939d187f6f5
shenglimdev <69244497+shenglimdev@users.noreply.github.com>
2020-08-11 17:41:00 +1000

    Made GuessingGame

---
+++ Chapter2 - Trip to objectville/TheGuessingGame/Player.java
@@ -0,0 +1,10 @@
+package TheGuessingGame;
+
+public class Player {
+    public int number = 0;
+
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("I'm guessing " + number);
+    }
+}
