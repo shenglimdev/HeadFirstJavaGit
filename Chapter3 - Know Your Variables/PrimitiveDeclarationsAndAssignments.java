70b2b2ecd6a6ddd3ebc785ecdbe4798d38e1e638
shenglimdev <69244497+shenglimdev@users.noreply.github.com>
2020-08-17 18:05:58 +1000

    Create PrimitiveDeclarationsAndAssignments.java

---
+++ Chapter3 - Know Your Variables/PrimitiveDeclarationsAndAssignments.java
@@ -0,0 +1,28 @@
+public class PrimitiveDeclarationsAndAssignments{
+
+    public static void main (String[] args){
+        int x;
+        x = 234;
+
+        byte b = 89;
+
+        boolean isFun = true;
+
+        double d = 3456.98;
+
+        char c = 'f';
+
+        int z = x;
+
+        boolean isPunkRock;
+        isPunkRock = false;
+
+        boolean powerOn;
+        powerOn = isFun;
+
+        long big = 3456789;
+
+        float f = 32.5f;
+        // note: float needs to put f if not java will think anything with decimal is a double (unless you use "f")
+    }
+}
