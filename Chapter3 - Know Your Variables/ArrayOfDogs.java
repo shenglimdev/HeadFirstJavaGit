5398c381f3b4d020e4e9b45f89970dc7d7aee9fa
shenglimdev <69244497+shenglimdev@users.noreply.github.com>
2020-08-18 16:10:11 +1000

    Create ArrayOfDogs.java

---
+++ Chapter3 - Know Your Variables/ArrayOfDogs.java
@@ -0,0 +1,41 @@
+class Dog {
+    String name;
+    public Dog(){
+
+    }
+
+    public void bark(){
+        System.out.println("Bork");
+    }
+
+    public void eat(){
+        System.out.println("Nom");
+    }
+
+    public void chaseCat(){
+        System.out.println("Chase");
+    }
+}
+
+public class ArrayOfDogs {
+    public static void main(String[] args) {
+        Dog[] pets;
+
+        pets = new Dog[7];
+
+        for (int i = 0; i < 7; i++){
+            pets[i] = new Dog();
+        }
+
+        Dog fido = new Dog();
+        fido.name = "Fido";
+
+        fido.bark();
+        fido.chaseCat();
+
+        Dog[] myDogs = new Dog[3];
+        myDogs[0] = new Dog();
+        myDogs[0].name = "Dido";
+        myDogs[0].bark();
+    }
+}
